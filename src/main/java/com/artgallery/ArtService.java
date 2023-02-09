02fb9a30b5e56cacf8f468cba780171428a86c87
Ola <aleksandraziemak.az@gmail.com>
2023-02-09 15:44:15 +0100

    Show artists added

--- src/main/java/com/artgallery/ArtService.java
+++ src/main/java/com/artgallery/ArtService.java
@@ -1,7 +1,9 @@
 package com.artgallery;
 
+import com.artgallery.model.Author;
 import com.artgallery.model.Painting;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -10,8 +12,17 @@ public class ArtService {
     private static final ArtRepository repository = new ArtRepository();
 
     public void showMyCollection() {
-        List<Painting> paintings = repository.getPaintingsFromJsonFile();
+        List<Painting> paintings = repository.getPaintings();
         IntStream.range(0, paintings.size())
                 .forEach(index -> System.out.println((index + 1) + ". " + paintings.get(index).getFullDescription()));
+    }
+
+    public void showMyArtists() {
+        List<Author> authors = repository.getPaintings().stream()
+                .map(Painting::getAuthor)
+                .distinct()
+                .toList();
+        IntStream.range(0, authors.size())
+                .forEach(index -> System.out.println((index + 1) +". " + authors.get(index).getDescription()));
     }
 }
