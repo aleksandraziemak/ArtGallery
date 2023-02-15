5ade41f85aecfbcbe4f030ea36634192461b7d04
Ola <aleksandraziemak.az@gmail.com>
2023-02-15 11:16:32 +0100

    Buy Painting added

--- src/main/java/com/artgallery/ArtService.java
+++ src/main/java/com/artgallery/ArtService.java
@@ -2,14 +2,17 @@ package com.artgallery;
 
 import com.artgallery.model.Author;
 import com.artgallery.model.Painting;
+import com.artgallery.util.UuidGeneratorUtil;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class ArtService {
 
     private static final ArtRepository repository = new ArtRepository();
+    Scanner scanner = new Scanner(System.in);
 
     public void showMyCollection() {
         List<Painting> paintings = repository.getPaintings();
@@ -24,5 +27,27 @@ public class ArtService {
                 .toList();
         IntStream.range(0, authors.size())
                 .forEach(index -> System.out.println((index + 1) +". " + authors.get(index).getDescription()));
+    }
+
+    public void buyPainting() {
+        Painting painting = new Painting(UuidGeneratorUtil.uuidGenerate());
+        Author author = new Author();
+        System.out.println("Buying new painting...");
+        System.out.println("Set author data");
+        System.out.println("Firstname: ");
+        author.setFirstName(scanner.nextLine());
+        System.out.println("Lastname: ");
+        author.setLastName(scanner.nextLine());
+        System.out.println("Country: ");
+        author.setCountry(scanner.nextLine());
+        System.out.println("Set painting data");
+        System.out.println("Title: ");
+        painting.setTitle(scanner.nextLine());
+        System.out.println("Movement: ");
+        painting.setMovement(scanner.nextLine());
+        System.out.println("Year: ");
+        painting.setYear(scanner.nextLong());
+        painting.setAuthor(author);
+        repository.addPainting(painting);
     }
 }
