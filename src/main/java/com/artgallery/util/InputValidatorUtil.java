3ac83d9673b42c825d4fdb03ec5d1c019af70f21
Ola <aleksandraziemak.az@gmail.com>
2023-02-27 13:32:48 +0100

    Tests added

--- src/main/java/com/artgallery/util/InputValidatorUtil.java
+++ src/main/java/com/artgallery/util/InputValidatorUtil.java
@@ -7,50 +7,50 @@ import java.util.List;
 
 public class InputValidatorUtil {
 
-    public static boolean validate(String input, InputValidator validator) {
+    public static boolean isValid(String input, InputValidator validator) {
         return switch (validator) {
-            case YES_NO -> yesNoValidate(input);
-            case AUTHOR -> authorValidate(input);
-            case PAINTING -> paintingValidate(input);
-            case SEARCH -> searchValidate(input);
+            case YES_NO -> isValidYesNoInput(input);
+            case AUTHOR -> isValidAuthorInput(input);
+            case PAINTING -> isValidPaintingInput(input);
+            case SEARCH -> isValidSearch(input);
         };
     }
 
-    public static boolean isInputInvalid(String input, List<Painting> paintings) {
-        boolean inputInvalid = !input.matches("[1-9]") || Integer.parseInt(input) < 1 || Integer.parseInt(input) > paintings.size();
-        if (inputInvalid) {
+    public static boolean isInputValid(String input, List<Painting> paintings) {
+        boolean inputValid = input.matches("[1-9]") && Integer.parseInt(input) > 0 && Integer.parseInt(input) <= paintings.size();
+        if (!inputValid) {
             System.out.println("Wrong input, please choose number between 1 and " + paintings.size());
         }
-        return inputInvalid;
+        return inputValid;
     }
 
-    public static boolean yesNoValidate(String input) {
-        boolean inputInvalid = !input.matches("[yn]");
-        if (inputInvalid) {
+    public static boolean isValidYesNoInput(String input) {
+        boolean inputInvalid = input.matches("[yn]");
+        if (!inputInvalid) {
             System.out.println("Wrong input, choose: [y]es / [n]o");
         }
         return inputInvalid;
     }
 
-    public static boolean paintingValidate(String input) {
-        boolean inputInvalid = !input.matches("[txm]");
-        if (inputInvalid) {
+    public static boolean isValidPaintingInput(String input) {
+        boolean inputInvalid = input.matches("[txm]");
+        if (!inputInvalid) {
             System.out.println("Wrong input, choose: [t]itle / [x]year / [m]ovement");
         }
         return inputInvalid;
     }
 
-    public static boolean authorValidate(String input) {
-        boolean inputInvalid = !input.matches("[fslc]");
-        if (inputInvalid) {
+    public static boolean isValidAuthorInput(String input) {
+        boolean inputInvalid = input.matches("[fslc]");
+        if (!inputInvalid) {
             System.out.println("Wrong input, choose: [f]irst name / [s]econd name / [l]ast name / [c]ountry");
         }
         return inputInvalid;
     }
 
-    public static boolean searchValidate(String input) {
-        boolean inputInvalid = !input.matches("[lt]");
-        if (inputInvalid) {
+    public static boolean isValidSearch(String input) {
+        boolean inputInvalid = input.matches("[lt]");
+        if (!inputInvalid) {
             System.out.println("Wrong input, choose: [l]ast name of an author / [t]itle of a painting");
         }
         return inputInvalid;
